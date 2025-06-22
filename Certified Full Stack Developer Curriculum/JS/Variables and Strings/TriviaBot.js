728e2efe69dddb7f08fd3e92c56622042d48a445
Avion <avionsheadesigns@gmail.com>
2025-06-21 22:58:02 -0400

    completed trivia bot lab.

---
+++ Certified Full Stack Developer Curriculum/JS/Variables and Strings/TriviaBot.js
@@ -0,0 +1,23 @@
+const botName = "Brady";
+const botLocation = "Earth";
+const favoriteLanguage = "JavaScript";
+
+let codingFact = favoriteLanguage + " was created in May 1995.";
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was created by Brendan Eich in just 10 days.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was originally developed for Netscape Navigator, which was one of the first web browsers.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
