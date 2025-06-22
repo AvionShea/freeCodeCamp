726dd823d9ff6cf911334f5426681a48d26e6a80
Avion <avionsheadesigns@gmail.com>
2025-06-22 14:24:54 -0400

    completed sentence maker lab

---
+++ Certified Full Stack Developer Curriculum/SentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "big",
+    noun = "dog",
+    verb = "ran",
+    place = "Dave and Busters",
+    adjective2 = "purple",
+    noun2 = "sweet potatoes";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+console.log("First story: " + firstStory);
+
+adjective = "dark";
+noun = "bird";
+verb = "jumped";
+place = "Best Buy";
+adjective2 = "short";
+noun2 = "pumpkin seeds";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+console.log("Second story: " + secondStory);
